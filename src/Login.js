ccf2c18dfdb62dde975d689fc07a8d42f1814c02
Ashwin Dev <ashwindev1462001@gmail.com>
2023-02-18 16:42:40 +0530

    login and registration

--- src/Login.js
+++ src/Login.js
@@ -3,90 +3,141 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const Login = () => {
-    const [username, usernameupdate] = useState('');
-    const [password, passwordupdate] = useState('');
+  const [username, usernameupdate] = useState("");
+  const [password, passwordupdate] = useState("");
 
-    const usenavigate=useNavigate();
+  const usenavigate = useNavigate();
 
-    useEffect(()=>{
-sessionStorage.clear();
-    },[]);
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
 
-    const ProceedLogin = (e) => {
-        e.preventDefault();
-        if (validate()) {
-            ///implentation
-            // console.log('proceed');
-            fetch("http://localhost:5000/api/user/" + username).then((res) => {
-                return res.json();
-            }).then((resp) => {
-                console.log(resp)
-                if (Object.keys(resp).length === 0) {
-                    toast.error('Please Enter valid username');
-                } else {
-                    console.log(resp.userType);
-                    if ((resp.password === password) && (resp.userType==='0')) {
-                        toast.success('Succesfully Logged in.');
-                        usenavigate('/farmer-Dash');
-                        const userid = resp.id
-                        sessionStorage.setItem('userid',userid);
-                        
-                    }
-                    else if ((resp.password === password) && (resp.userType==='1')){
-                        toast.success('Succesfully Logged in.');
-                        const userid = resp.id
-                        sessionStorage.setItem('userid',userid);
-                        usenavigate('/consumer-Dash')
-                    }
-                    else{
-                        toast.error('Please Enter valid credentials');
-                    }
-                }
-            }).catch((err) => {
-                toast.error('Login Failed due to :' + err.message);
-            });
-        }
+  const ProceedLogin = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      ///implentation
+      // console.log('proceed');
+
+      const regobj = {
+        email: username,
+        password,
+      };
+
+      const url = process.env.REACT_APP_BACKEND_URL + "/api/user/login";
+      fetch(url, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(regobj),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((resp) => {
+          console.log(resp);
+
+          if (resp.status !== 200) {
+            var err = new Error();
+            err.message = resp.message;
+            throw err;
+          } else {
+            toast.success("Succesfully Logged in.");
+            const userid = resp.user._id;
+            const jwt = resp.user.jwt;
+            sessionStorage.setItem("userid", userid);
+            sessionStorage.setItem("jwt", jwt);
+            if (resp.user.type === 0) {
+              //farmer
+              usenavigate("/farmer-Dash");
+            } else {
+              usenavigate("/consumer-Dash");
+            }
+          }
+
+          /*
+          if (Object.keys(resp).length === 0) {
+            toast.error("Please Enter valid username");
+          } else {
+            console.log(resp.userType);
+            if (resp.password === password && resp.userType === "0") {
+              toast.success("Succesfully Logged in.");
+              usenavigate("/farmer-Dash");
+              const userid = resp.id;
+              sessionStorage.setItem("userid", userid);
+            } else if (resp.password === password && resp.userType === "1") {
+              toast.success("Succesfully Logged in.");
+              const userid = resp.id;
+              sessionStorage.setItem("userid", userid);
+              usenavigate("/consumer-Dash");
+            } else {
+              toast.error("Please Enter valid credentials");
+            }
+          }
+          */
+        })
+        .catch((err) => {
+          toast.error(err.message);
+        });
     }
-    
-    const validate = () => {
-        let result = true;
-        if (username === '' || username === null) {
-            result = false;
-            toast.warning('Please Enter Username');
-        }
-        if (password === '' || password === null) {
-            result = false;
-            toast.warning('Please Enter Password');
-        }
-        return result;
+  };
+
+  const validate = () => {
+    let result = true;
+    if (username === "" || username === null) {
+      result = false;
+      toast.warning("Please Enter Username");
     }
-    return (
-        <div className="row">
-            <div className="offset-lg-3 col-lg-6" style={{ marginTop: '100px' }}>
-                <form onSubmit={ProceedLogin} className="container">
-                    <div className="card">
-                        <div className="card-header">
-                            <h2>Login</h2>
-                        </div>
-                        <div className="card-body">
-                            <div className="form-group">
-                                <label>User Name <span className="errmsg">*</span></label>
-                                <input value={username} onChange={e => usernameupdate(e.target.value)} className="form-control"></input>
-                            </div>
-                            <div className="form-group">
-                                <label>Password <span className="errmsg">*</span></label>
-                                <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control"></input>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">Login</button> |
-                            <Link className="btn btn-success" to={'/register'}>New User</Link>
-                        </div>
-                    </div>
-                </form>
+    if (password === "" || password === null) {
+      result = false;
+      toast.warning("Please Enter Password");
+    }
+    return result;
+  };
+  return (
+    <div className="row">
+      <div className="offset-lg-3 col-lg-6" style={{ marginTop: "100px" }}>
+        <form onSubmit={ProceedLogin} className="container">
+          <div className="card">
+            <div className="card-header">
+              <h2>Login</h2>
             </div>
-        </div>
-    );
-}
+            <div className="card-body">
+              <div className="form-group">
+                <label>
+                  Email <span className="errmsg">*</span>
+                </label>
+                <input
+                  value={username}
+                  onChange={(e) => usernameupdate(e.target.value)}
+                  className="form-control"
+                  type="email"
+                ></input>
+              </div>
+              <div className="form-group">
+                <label>
+                  Password <span className="errmsg">*</span>
+                </label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => passwordupdate(e.target.value)}
+                  className="form-control"
+                ></input>
+              </div>
+            </div>
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>{" "}
+              |
+              <Link className="btn btn-success" to={"/register"}>
+                New User
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
