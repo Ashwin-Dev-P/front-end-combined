08068c51cc25d9a83040c8492941fbd3ecfff850
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-02-22 15:43:41 +0530

    update location

--- src/component/farmer/FarmersProfile.jsx
+++ src/component/farmer/FarmersProfile.jsx
@@ -1,4 +1,6 @@
+
 import React, { useState, useEffect } from "react";
+import MyButton from "../../MyComponents/MyButton";
 import FarmersNav from "./FarmersNav";
 
 function FarmersProfile() {
@@ -13,6 +15,30 @@ function FarmersProfile() {
       .then((data) => setUser(data.profile_details));
   }, []);
   console.log(user.email);
+
+  const updateLocation=(e)=>{
+    e.preventDefault();
+    const location = document.getElementById("location").value;
+    const url = process.env.REACT_APP_BACKEND_URL + "/api/user/location";
+
+    const regobj = {
+      location,
+      user_id: sessionStorage.getItem("userid"),
+    };
+
+      fetch(url, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(regobj),
+      })
+        .then((res) => {
+          
+          return res.json();
+        })
+        .then((res=>{
+          console.log(res)
+        }))
+  }
   return (
     <section className="farmer-profile_body">
       <FarmersNav />
@@ -22,6 +48,20 @@ function FarmersProfile() {
         <p className="name">Email ID: {user.email}</p>
         <p className="name">Phone: {user.phone_number}</p>
         <p className="name">Address: {user.address}</p>
+      </div>
+
+      <div className="row mt-5">
+        <div className="col-xs-12 col-md-3"></div>
+        <div className="col-xs-12 col-md-6 text-center ">
+
+        <form onSubmit={updateLocation} className="form">
+        <div className="form-group">
+          <input type="url" id="location" className="form-control" placeholder="https://goo.gl/maps/" />
+          </div>
+          <MyButton text="Update location" className="btn btn-primary mt-1"  />
+        </form>
+        </div>
+        
       </div>
     </section>
   );
