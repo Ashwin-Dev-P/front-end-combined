707f4f865bbd7fdbd0c770985dbdb53118dcd184
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-02-23 11:02:42 +0530

    changes

--- src/component/components/FarmerItem.js
+++ src/component/components/FarmerItem.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class FarmerItem extends Component {
-  render() {
-    const { username , _id } = this.props.farmer;
-    return (
-      <Link  to={`/consumer/farmer/${_id}`} className='border shadow-sm py-2 text-decoration-none text-center'>
-        
-        {username}
-        
-      </Link>
-    )
-  }
+	render() {
+		const { username, _id } = this.props.farmer;
+		return (
+			<Link
+				to={`/consumer/farmer/${_id}`}
+				className="border shadow-sm py-2 text-decoration-none text-center"
+			>
+				{username}
+			</Link>
+		);
+	}
 }
--- src/component/consumerPages/ConsumerDashboard.jsx
+++ src/component/consumerPages/ConsumerDashboard.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ConsumerDashboard = () => {
-  return (
-    <section className="consumer-dashboard_body">
-      <div className="consumer-dashboard_body--content">
-        <h1>agromart</h1>
-        <p className="consumer-dashboard_body--smallcontent">
-          Shop Your Produce As Farm Fresh Online!
-        </p>
-        <Link to={'/consumer/shop'} className="shop-btn"> Shop Now!</Link>
-        <Link to={"/consumer/farmers"} className="shop-btn mt-3 ">Search by farmers</Link>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="consumer-dashboard_body">
+			<div className="consumer-dashboard_body--content">
+				<h1>agromart</h1>
+				<p className="consumer-dashboard_body--smallcontent">
+					Shop Your Produce As Farm Fresh Online!
+				</p>
+				<Link to={"/consumer/shop"} className="shop-btn">
+					{" "}
+					Shop by Products
+				</Link>
+				<Link to={"/consumer/farmers"} className="shop-btn mt-3 ">
+					Shop by farmers
+				</Link>
+			</div>
+		</section>
+	);
+};
 
-export default ConsumerDashboard
+export default ConsumerDashboard;
